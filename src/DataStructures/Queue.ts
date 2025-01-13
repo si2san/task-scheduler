d2666881faab966641ccb47e295593b945273f43
sithusan <sithusan@hivelocity.co.jp>
2025-01-13 14:34:34 +0700

    build: basic test setup with jest

---
+++ src/DataStructures/Queue.ts
@@ -0,0 +1,5 @@
+export class Queue<T> {
+    isEmpty(): boolean {
+        return true;
+    }
+}
---
+++ src/tests/DataStructures/Queue.test.ts
@@ -0,0 +1,14 @@
+import {Queue} from "../../DataStructures/Queue";
+
+
+describe("Queue", () => {
+    let queue: Queue<number>;
+
+    beforeEach(() => {
+        queue = new Queue<number>();
+    })
+
+    test('should be empty', () => {
+        expect(queue.isEmpty()).toBe(true);
+    });
+});
